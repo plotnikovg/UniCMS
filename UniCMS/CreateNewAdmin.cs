60ec2dddfe66b9658a2393198e4a94e55328df3d
plotnikovg <84013482+plotnikovg@users.noreply.github.com>
2021-05-11 17:56:20 +0300

    nothing

---
+++ UniCMS/CreateNewAdmin.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UniCMS.Data.Models;
+
+namespace UniCMS
+{
+  public class CreateNewAdmin
+  {
+    public static void NewAdmin(ApplicationContext context)
+    {
+      if (!context.Users.Any())
+      {
+        context.Users.Add(new User
+        {
+          UserName = "admin",
+          Password = "12345"
+        });
+        context.SaveChanges();
+      }
+    }
+  }
+}
--- UniCMS/Startup.cs
+++ UniCMS/Startup.cs
@@ -44,6 +44,11 @@ namespace UniCMS
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
     {
+      using (var serviceScope = app.ApplicationServices.GetService<IServiceScopeFactory>().CreateScope())
+      {
+        var context = serviceScope.ServiceProvider.GetRequiredService<ApplicationContext>();
+        CreateNewAdmin.NewAdmin(context);
+      }
       if (env.IsDevelopment())
       {
         app.UseDeveloperExceptionPage();
